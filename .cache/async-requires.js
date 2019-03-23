06806c029ed356de9bd3ca8866fc1bcc3135b06b
James DeLay <jdelay.jr@gmail.com>
2019-03-23 03:22:28 -0400

    removing about

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -7,7 +7,6 @@ exports.components = {
   "component---src-templates-author-tsx": () => import("/home/jim/Documents/BlogSite_2.0/src/templates/author.tsx" /* webpackChunkName: "component---src-templates-author-tsx" */),
   "component---cache-dev-404-page-js": () => import("/home/jim/Documents/BlogSite_2.0/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-tsx": () => import("/home/jim/Documents/BlogSite_2.0/src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
-  "component---src-pages-about-tsx": () => import("/home/jim/Documents/BlogSite_2.0/src/pages/about.tsx" /* webpackChunkName: "component---src-pages-about-tsx" */),
   "component---src-pages-index-tsx": () => import("/home/jim/Documents/BlogSite_2.0/src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -10,7 +10,6 @@ exports.components = {
   "component---src-templates-author-tsx": hot(preferDefault(require("/home/jim/Documents/BlogSite_2.0/src/templates/author.tsx"))),
   "component---cache-dev-404-page-js": hot(preferDefault(require("/home/jim/Documents/BlogSite_2.0/.cache/dev-404-page.js"))),
   "component---src-pages-404-tsx": hot(preferDefault(require("/home/jim/Documents/BlogSite_2.0/src/pages/404.tsx"))),
-  "component---src-pages-about-tsx": hot(preferDefault(require("/home/jim/Documents/BlogSite_2.0/src/pages/about.tsx"))),
   "component---src-pages-index-tsx": hot(preferDefault(require("/home/jim/Documents/BlogSite_2.0/src/pages/index.tsx")))
 }
 
