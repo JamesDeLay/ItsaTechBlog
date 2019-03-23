d91045e18a52807cb22449f8e8121b2dabda2b6c
James DeLay <jdelay.jr@gmail.com>
2019-03-23 01:48:37 -0400

    added my headshot

--- src/website-config.ts
+++ src/website-config.ts
@@ -34,7 +34,7 @@ const config: WebsiteConfig = {
   logo: 'img/logo.png',
   lang: 'en',
   siteUrl: 'https://gatsby-casper.netlify.com',
-  twitter: 'https://twitter.com/tryghost',
+  twitter: 'https://twitter.com/blog_itsa',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
