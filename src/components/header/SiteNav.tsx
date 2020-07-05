13f540b8d5aa9b27f0d622522a0a0198925bf1fb
James DeLay <James_DeLay@mckinsey.com>
2020-07-05 16:04:42 -0400

    feat: updating headshot and author bios

--- src/components/header/SiteNav.tsx
+++ src/components/header/SiteNav.tsx
@@ -148,15 +148,12 @@ class SiteNav extends React.Component<SiteNavProps, SiteNaveState> {
           {!isHome && <SiteNavLogo />}
           <ul css={NavStyles} role="menu">
             {/* TODO: mark current nav item - add class nav-current */}
-            {/* <li role="menuitem">
-              <Link to="/">Home</Link>
-            </li> */}
-            {/* <li role="menuitem">
+            <li role="menuitem">
               <Link to="/about">About</Link>
             </li>
             <li role="menuitem">
-              <Link to="/tags/getting-started/">Getting Started</Link>
-            </li> */}
+              <Link to="/tags/getting-started/">Resume</Link>
+            </li>
           </ul>
         </SiteNavLeft>
         <SiteNavRight>
